b84504c08d30316ddef867a108a48520df5aa580
Schlomo Schapiro <schlomo.schapiro@zalando.de>
2017-09-15 13:38:39 +0200

    local dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
 
 gulp.task('clean', function() {
   return require('del')(['out']);
@@ -8,5 +9,16 @@ gulp.task('build', ['clean'], function(){
   return gulp.src(['src/**'])
     .pipe(gulp.dest('out'));
 })
+
+gulp.task('apimocker', ['build'], function(){
+  gulp.watch('src/**', ['build'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+  return $.apimocker.start({
+    staticDirectory: 'out',
+    staticPath: '/'
+  });
+});
 
 gulp.task('default', ['build']);
